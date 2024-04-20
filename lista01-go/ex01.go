96bf985f4cb76a5bcfdbba33ba6a1791a7583bb7
luizaugusto01 <166179636+luizaugusto01@users.noreply.github.com>
2024-04-19 21:19:31 -0300

    aula

--- lista01-go/ex01.go
+++ lista01-go/ex01.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    var notasString string
-    fmt.Println("Digite as três notas do aluno separadas por espaço:")
-    fmt.Scanln(&notasString)
+	var notasString string
+	fmt.Println("Digite as três notas do aluno separadas por espaço:")
+	fmt.Scanln(&notasString)
 
-    notas := strings.Split(notasString, " ")
-    var total float64
-    for _, nota := range notas {
-        notaFloat, _ := strconv.ParseFloat(nota, 64)
-        total += notaFloat
-    }
+	notas := strings.Split(notasString, " ")
+	var total float64
+	for _, nota := range notas {
+		notaFloat, _ := strconv.ParseFloat(nota, 64)
+		total += notaFloat
+	}
 
-    media := total / float64(len(notas))
-    fmt.Printf("MEDIA = %.2f\n", media)
+	media := total / float64(len(notas))
+	fmt.Printf("MEDIA = %.2f\n", media)
 
-    if media >= 6.0 {
-        fmt.Println("APROVADO")
-    } else {
-        fmt.Println("REPROVADO")
-    }
+	if media >= 6.0 {
+		fmt.Println("APROVADO")
+	} else {
+		fmt.Println("REPROVADO")
+	}
 }
