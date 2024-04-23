9ebbea006e5c5bd6a8e9c3fda8a0ec0b864170bb
luiz <you@example.com>
2024-04-23 19:35:00 -0300

    .

--- lista01-go/ex03.go
+++ lista01-go/ex03.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println("digite os números a, b, c ")
 	fmt.Scan(&n1, &n2, &n3)
 
-	if n1 < 0 && n1 > 9 && n2 < 0 && n2 > 9 && n3 < 0 && n3 > 9 {
+	if n1 < 0 || n1 > 9 || n2 < 0 || n2 > 9 || n3 < 0 && n3 > 9 {
 		fmt.Println("digito invalido")
 		return
 	}
