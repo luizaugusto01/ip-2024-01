c80a94b62bc763d65746971f482fff35c44a282a
luiz <you@example.com>
2024-04-23 21:14:39 -0300

    .

--- lista01-go/ex05.go
+++ lista01-go/ex05.go
@@ -2,35 +2,39 @@ package main
 
 import "fmt"
 
+var contaCliente int
+var consumoAgua float64
+var tipoConsumidor int
+
 func main() {
-    var contaCliente int
-    var consumoAgua float64
-    var tipoConsumidor rune
 
-    // Leitura da entrada: conta do cliente, consumo de água e tipo de consumidor
-    fmt.Println("Digite a conta do cliente, o consumo de água (em metros cúbicos) e o tipo de consumidor (R - Residencial, C - Comercial, I - Industrial), separados por espaço:")
-    fmt.Scanln(&contaCliente, &consumoAgua, &tipoConsumidor)
+	// Leitura da entrada: conta do cliente, consumo de água e tipo de consumidor
+	fmt.Println("Digite a conta do cliente, o consumo de água (em metros cúbicos) e o tipo de consumidor (1- Residencial, 2 - Comercial, 3 - Industrial), separados por espaço:")
+	fmt.Scanln(&contaCliente, &consumoAgua, &tipoConsumidor)
 
-    // Calcula o valor da conta com base no tipo de consumidor
-    var valorConta float64
-    switch tipoConsumidor {
-    case 'R':
-        valorConta = 5.0 + (0.05 * consumoAgua)
-    case 'C':
-        if consumoAgua <= 80 {
-            valorConta = 500.0
-        } else {
-            valorConta = 500.0 + (0.25 * (consumoAgua - 80))
-        }
-    case 'I':
-        if consumoAgua <= 100 {
-            valorConta = 800.0
-        } else {
-            valorConta = 800.0 + (0.04 * (consumoAgua - 100))
-        }
-    }
+	// Calcula o valor da conta com base no tipo de consumidor
+	var valorConta float64
+	switch tipoConsumidor {
+	case 1:
+		valorConta = 5.0 + (0.05 * consumoAgua)
+	case 2:
+		if consumoAgua <= 80 {
+			valorConta = 500.0
+		} else {
+			valorConta = 500.0 + (0.25 * (consumoAgua - 80))
+		}
+	case 3:
+		if consumoAgua <= 100 {
+			valorConta = 800.0
+		} else {
+			valorConta = 800.0 + (0.04 * (consumoAgua - 100))
+		}
+	default:
+		fmt.Printf("digito invalido")
+		return
+	}
 
-    // Imprime a conta do cliente e o valor a ser pago
-    fmt.Printf("CONTA = %d\n", contaCliente)
-    fmt.Printf("VALOR DA CONTA = %.2f\n", valorConta)
+	// Imprime a conta do cliente e o valor a ser pago
+	fmt.Printf("CONTA = %d\n", contaCliente)
+	fmt.Printf("VALOR DA CONTA = %.2f\n", valorConta)
 }
--- lista01-go/ex06.go
+++ lista01-go/ex06.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    var n int
-    fmt.Println("Digite o número de temperaturas em Fahrenheit a serem convertidas:")
-    fmt.Scanln(&n)
+	var n int
+	fmt.Println("Digite o número de temperaturas em Fahrenheit a serem convertidas:")
+	fmt.Scanln(&n)
 
-    for i := 0; i < n; i++ {
-        var fahrenheit float64
-        fmt.Printf("Digite a temperatura em Fahrenheit %d: ", i+1)
-        fmt.Scanln(&fahrenheit)
+	for i := 1; i <= n; i++ {
+		var fahrenheit float64
+		fmt.Printf("Digite a temperatura em Fahrenheit %v: ", i)
+		fmt.Scanln(&fahrenheit)
 
-        // Converte Fahrenheit para Celsius
-        celsius := (fahrenheit - 32) * 5 / 9
+		// Converte Fahrenheit para Celsius
+		celsius := (fahrenheit - 32) * 5 / 9
 
-        // Imprime a temperatura convertida
-        fmt.Printf("%.2f FAHRENHEIT EQUIVALE A %.2f CELSIUS\n", fahrenheit, celsius)
-    }
+		// Imprime a temperatura convertida
+		fmt.Printf("%.2f FAHRENHEIT EQUIVALE A %.2f CELSIUS\n", fahrenheit, celsius)
+	}
 }
